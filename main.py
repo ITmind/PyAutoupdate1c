55235a465d3299ded5937f816115332947fa46d8
ITmindCo <itmindco@gmail.com>
2020-02-03 14:49:14 +1000

    отправка почты и новый файл настроек

--- main.py
+++ main.py
@@ -3,33 +3,31 @@ import pyuac
 from v8 import V83
 import os
 import sys
+import json
 import logging
 import time
+from io import StringIO
 from smtplib import SMTP
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+_stringStream = StringIO()
+
+
 def main():
-    # serverName = 'c2-it-s-1c'
-    serverName = '192.168.0.11'
-    baseNames = ['buhcopy', 'buh', 'buhreg']
+
+    config = readConfig()
+    if config is None:
+        logging.warning('config.json created. restart app')
+        return
 
     logging.info('start PyAutoupdate1c')
-    auth = {}
-    passFile = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), 'pass.txt'))
-    logging.debug(passFile)
-    with open(passFile, encoding='utf-8') as file:
-        for line in file:
-            line = line.rstrip('\n')
-            key, val = line.split(':')
-            auth[key] = val
 
-    _1c = V83(serverName, auth)
-    for baseName in baseNames:
+    _1c = V83(config['server'], config['1cpass'])
+    for baseName in config['bases']:
         if _1c.checkModify(baseName, 'Администратор'):
             logging.info(f'{baseName} modify')
-            _1c = V83(serverName, auth)
+
             if _1c.enableSessionDenied(baseName):
                 _1c.restart1cService()
                 _1c.updateConfig(baseName, 'Администратор')
@@ -39,27 +37,60 @@ def main():
             logging.info(f'{baseName} not modify')
 
     logging.info('end')
-    
-    message = logging.handlers[1]
+
+    message = _stringStream.getvalue()
     msg = MIMEMultipart()
-    msg['From'] = "your_address"
-    msg['To'] = "to_address"
-    msg['Subject'] = "Photos"
+    msg['From'] = config['mail']['from']
+    msg['To'] = config['mail']['to']
+    msg['Subject'] = "PyAutoupdate1c"
     msg.attach(MIMEText(message, 'plain'))
 
-    # with SMTP("smtp.gmail.com: 587") as smtp:
-    #     smtp.sendmail(msg['From'], msg['To'], msg.as_string())
-
+    with SMTP(f"{config['mail']['smtpserver']}: {config['mail']['smtpport']}") as smtp:
+        smtp.starttls()
+        smtp.login(msg['From'], config['mail']['pass'])
+        smtp.sendmail(msg['From'], msg['To'], msg.as_string())
 
 
 def logSetup():
     logFile = os.path.abspath(os.path.join(
         os.path.dirname(__file__), 'log.log'))
-    self.stream = StringIO()
     logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s',
                         handlers=[logging.FileHandler(
-                            logFile, encoding='utf-8'), logging.StreamHandler()],
+                            logFile, encoding='utf-8'),
+                            logging.StreamHandler(),
+                            logging.StreamHandler(_stringStream)],
                         level=logging.DEBUG)
+
+
+def readConfig():
+    configFile = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), 'config.json'))
+    logging.debug(configFile)
+    if not os.path.exists(configFile):
+        createDefConfig()
+        return None
+
+    with open(configFile, encoding='utf-8') as file:
+        config = json.load(file)
+
+    return config
+
+
+def createDefConfig():
+    configFile = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), 'config.json'))
+    logging.debug(configFile)
+
+    config = {}
+    config['server'] = '127.0.0.1'
+    config['bases'] = ['ut11', 'bp']
+    config['1cpass'] = {'user1': 'pass1', 'user2': 'pass2'}
+    config['mail'] = {'smtpserver': 'smtp.gmail.com',
+                      'smtpport': '587',
+                      'pass': 'password'}
+
+    with open(configFile, 'w', encoding='utf-8') as file:
+        json.dump(config, file)
 
 
 def runAsAdmin(func):
